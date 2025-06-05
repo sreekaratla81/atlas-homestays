488dbf57f3a73e770c1a41f5b3da9ac760ca771d
sreekaratla81 <150553417+sreekaratla81@users.noreply.github.com>
2025-06-05 17:40:19 +0530

    apply styles

--- apps/admin-portal/src/main.jsx
+++ apps/admin-portal/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
+import './style.css';
 
 const domain = "atlashomestays.us.auth0.com";
 const clientId = "d70OGzWag10f4viX8DI1SxOXAj6aDsvX";
