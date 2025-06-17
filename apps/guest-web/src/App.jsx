2faa00aad42836dc0a1ae26433cff9b67ee87cb2
sreekaratla81 <150553417+sreekaratla81@users.noreply.github.com>
2025-06-17 06:35:50 +0530

    remove booknow page

--- apps/guest-web/src/App.jsx
+++ apps/guest-web/src/App.jsx
@@ -1,16 +1,14 @@
 // guest-web/src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import BookNow from './pages/BookNow';
-import GuestBooking from './pages/GuestBooking'; // 👈 Add this
+import GuestBooking from './pages/GuestBooking';
 
 function App() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/book" element={<BookNow />} />
-                <Route path="/guest-booking" element={<GuestBooking />} /> {/* 👈 New route */}
+                <Route path="/guest-booking" element={<GuestBooking />} />
             </Routes>
         </>
     );
